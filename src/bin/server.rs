743f807882cd2e1f7fbfc12586b32543ffae3b6b
18leaf <18leaf@gmail.com>
2026-02-27 23:00:21 -0500

    updated name

--- src/bin/server.rs
+++ src/bin/server.rs
@@ -24,7 +24,7 @@ async fn index() -> Html<&'static str> {
 <html lang="en">
 <head>
     <meta charset="utf-8">
-    <title>FitApp</title>
+    <title>fitapp</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <style>
         :root {
